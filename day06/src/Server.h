d5ddd05eb69327fa7e1e166e58d4e23828e68c54
5yh <13951968938@126.com>
2024-12-09 17:10:23 +0800

    day 06

---
+++ day06/src/Server.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdio.h>
+#include <errno.h>
+class EventLoop;
+class Socket;
+class Server
+{
+private:
+    EventLoop *loop;
+
+public:
+    Server(EventLoop *);
+    ~Server();
+
+    void handleReadEvent(int);
+    void newConnection(Socket *serv_sock);
+};
