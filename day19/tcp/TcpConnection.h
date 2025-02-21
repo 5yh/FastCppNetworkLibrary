c7966442520015b13cb2c01734a68f3588540cfb
5yh <13951968938@126.com>
2025-02-21 20:17:00 +0800

    2

--- day19/tcp/TcpConnection.h
+++ day19/tcp/TcpConnection.h
@@ -5,7 +5,7 @@
 #include <memory>
 #include <string>
 class Buffer;
-
+class HttpContext;
 class TcpConnection : public std::enable_shared_from_this<TcpConnection>
 {
 public:
@@ -13,11 +13,12 @@ public:
     {
         Invalid = 1,
         Connected,
-        Disconnected
+        Disconected
     };
 
     DISALLOW_COPY_AND_MOVE(TcpConnection);
 
+    
     TcpConnection(EventLoop *loop, int connfd, int connid);
     ~TcpConnection();
 
@@ -26,40 +27,43 @@ public:
 
     // 销毁TcpConection
     void ConnectionDestructor();
+
     // 建立连接时调用回调函数
     void set_connection_callback(std::function<void(const std::shared_ptr<TcpConnection> &)> const &fn);
-    // 关闭时的回调函数
-    void set_close_callback(std::function<void(const std::shared_ptr<TcpConnection> &)> const &fn);
-    // 接受到信息的回调函数
-    void set_message_callback(std::function<void(const std::shared_ptr<TcpConnection> &)> const &fn);
+     // 关闭时的回调函数
+    void set_close_callback(std::function<void(const std::shared_ptr<TcpConnection> &)> const &fn);   
+    // 接受到信息的回调函数                                  
+    void set_message_callback(std::function<void(const std::shared_ptr<TcpConnection> &)> const &fn); 
 
     // 设定send buf
-    void set_send_buf(const char *str);
+    void set_send_buf(const char *str); 
     Buffer *read_buf();
     Buffer *send_buf();
 
-    void Read();                         // 读操作
-    void Write();                        // 写操作
-    void Send(const std::string &msg);   // 输出信息
+    void Read(); // 读操作
+    void Write(); // 写操作
+    void Send(const std::string &msg); // 输出信息
     void Send(const char *msg, int len); // 输出信息
     void Send(const char *msg);
+
 
     void HandleMessage(); // 当接收到信息时，进行回调
 
     // 当TcpConnection发起关闭请求时，进行回调，释放相应的socket.
-    void HandleClose();
-
+    void HandleClose(); 
     ConnectionState state() const;
     EventLoop *loop() const;
     int fd() const;
     int id() const;
+    HttpContext *context() const;
+
 
 private:
     // 该连接绑定的Socket
     int connfd_;
-    // 这个是什么？
-    // 没用上
+    // 为该连接分配一个id方便后续的debug
     int connid_;
+
     // 连接状态
     ConnectionState state_;
 
@@ -72,6 +76,9 @@ private:
     std::function<void(const std::shared_ptr<TcpConnection> &)> on_close_;
     std::function<void(const std::shared_ptr<TcpConnection> &)> on_message_;
     std::function<void(const std::shared_ptr<TcpConnection> &)> on_connect_;
+
     void ReadNonBlocking();
     void WriteNonBlocking();
+
+    std::unique_ptr<HttpContext> context_;
 };
